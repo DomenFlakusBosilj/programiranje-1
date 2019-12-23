3a5a8b65ba559779996e777150114477d2120993
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-12-23 12:03:12 +0100

    vaje 23.12.

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -16,6 +16,47 @@
  - : int = 13
 [*----------------------------------------------------------------------------*)
 
+let rec max_cheese cheese_matrix =
+  (* i for rows, j for columns *)
+  let max_i = Array.length cheese_matrix in 
+  let max_j = Array.length cheese_matrix.(0) in
+  let max_matrix = Array.make_matrix max_i max_j 0 in
+  let how_much_cheese i j =
+    let cheese = cheese_matrix.(i).(j) in
+    if i < (max_i - 1) then
+      (* gledamo tudi dol *)
+      if j < (max_j - 1) then
+        cheese + max (max_matrix.(i+1).(j)) (max_matrix.(i).(j+1))
+      else
+        cheese + max_matrix.(i+1).(j)
+    else
+      if j < (max_j - 1) then
+        cheese + max_matrix.(i).(j+1)
+      else
+        cheese
+  in
+
+  (* let how_much_cheese2 i j =
+    let cheese = cheese_matrix.(i).(j) in
+    let max_right = if j < (max_j - 1) then max_matrix.(i).(j+1) else 0 in
+    let max_down = if i < (max_i - 1) then max_matrix.(i+1).(j) else 0 in
+    cheese + max max_right max_down
+  in *)
+
+  let rec loop i j =
+    let cheese = how_much_cheese i j in
+    let () = max_matrix.(i).(j) <- cheese in
+    if j > 0 then
+    (* vse je v redu *)
+      loop i (j-1)
+    else
+    (* moremo skocit v novo vrstico *)
+      if i > 0 then
+        loop (i-1) (max_j - 1)
+      else ()
+  in
+  loop (max_i - 1) (max_j - 1); max_matrix.(0).(0)
+
 let test_matrix = 
   [| [| 1 ; 2 ; 0 |];
      [| 2 ; 4 ; 5 |];
@@ -37,6 +78,33 @@ let test_matrix =
  - : int = 35
 [*----------------------------------------------------------------------------*)
 
+let alternating_towers h =
+  (* make memory *)
+  let red_mem = Array.make (h+1) 0 in     (* red memory *)
+  let blue_mem = Array.make (h+1) 0 in
+  (* calculate one value by using recursion with memory *)
+  let red_towers = function
+    (* check bounds *)
+    | 0 -> 0
+    | 1 | 2 -> 1        (* 1 ali 2 naredi v 1 *)
+    | h -> blue_mem.(h-1) + blue_mem.(h-2)
+  in
+
+  let blue_towers = function
+    | 0 | 1 -> 0
+    | 2 -> 1 
+    | 3 -> 2    
+    | h -> red_mem.(h-2) + red_mem.(h-3)
+  in
+
+  let rec loop n =
+    if n > h then () else
+    let _ = red_mem.(n) <- red_towers n in
+    let _ = blue_mem.(n) <- blue_towers n in
+    loop (n+1)
+  in
+
+  let _ = loop 0 in red_mem.(h) + blue_mem.(h)
 
 (*----------------------------------------------------------------------------*]
  Na nagradni igri ste zadeli kupon, ki vam omogoča, da v Mercatorju kupite
